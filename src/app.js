439ed309931357762fd840ace8c0cefa3e0e8a43
Fionnuala12 <fionnuala.wilson@btinternet.com>
2021-01-15 22:10:51 +0000

    Time & Date added to JS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,23 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp); 
+    let hours = date.getHours(); 
+    if (hours < 10){
+        hours = `0${hour}`;}
+    let minutes = date.getMinutes(); 
+    if (minutes < 10){
+        minutes = `0${minuets}`;}
+    let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+    let day = days[date.getDay()]; 
+    return `${day}, ${hours}:${minutes}`;
+
+}
+
+
 function showTemperature(response){
     console.log(response.data);
+    let city = (response.data.name); 
+    let cityName = document.querySelector("#city");
+    cityName.innerHTML = city;
     let temperature = document.querySelector("#temperature"); 
     temperature.innerHTML = Math.round(response.data.main.temp);
     let windSpeed = document.querySelector("#wind"); 
@@ -11,13 +29,13 @@ function showTemperature(response){
     let maxTemp = document.querySelector("#max");
     maxTemp.innerHTML = Math.round(response.data.main.temp_max);
     let minTemp = document.querySelector("#min");
-    minTemp.innerHTML = Math.round(response.data.main.temp_min);
+    minTemp.innerHTML = Math.round(response.data.main.temp_min);  
+    let dateElement = document.querySelector("#date"); 
+    dateElement.innerHTML = formatDate(response.data.dt * 1000); 
 }
-
 
 let apiKey = "06c9d19d30f0be8b128071a6b5e0aeb3"; 
 let units = "metric";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=${units}`;
-console.log(apiUrl);
 
 axios.get(apiUrl).then(showTemperature);
